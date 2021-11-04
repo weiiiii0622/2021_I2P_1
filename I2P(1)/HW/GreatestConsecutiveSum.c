7765699eefcd36e6c40f24c32cdf230cf3da966c
weiiiii0622 <weiiiii.0622@gmail.com>
2021-11-04 17:50:42 +0800

    update HW

--- I2P(1)/HW/GreatestConsecutiveSum.c
+++ I2P(1)/HW/GreatestConsecutiveSum.c
@@ -5,8 +5,26 @@ int main(){
     int N;
     scanf("%d", &N);
 
-    
+    int nums[N];
+    for(int i=0; i<N; i++){
+        scanf("%d", &nums[i]);
+    }
 
+    int now = nums[0];
+    int max = nums[0];
+
+    for(int i=1; i<N; i++){
+        if(now < 0){
+            now = nums[i];
+        }
+        else{
+            now += nums[i];
+        }
+        if(now > max){
+            max = now;
+        }
+    }
+    printf("%d\n", max);
 
     return 0;
 }
