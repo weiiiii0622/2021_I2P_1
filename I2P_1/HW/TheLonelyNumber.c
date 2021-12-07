ed05aa7b02fd352437d9aba1d6f1341c81d7b237
weiiiii0622 <weiiiii.0622@gmail.com>
2021-12-07 20:32:10 +0800

    update The Lonely Number

--- I2P_1/HW/TheLonelyNumber.c
+++ I2P_1/HW/TheLonelyNumber.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int N, count, die;
-int nums[50];
-int status[50];
+int nums[301];
+int status[301];
 
-int find(int, int, int);
+void find(int, int);
 void bubble_sort(int*);
 
 int main(){
@@ -17,11 +17,7 @@ int main(){
 
     bubble_sort(nums);
 
-    for(int i=N-1; i>=0; i--){
-        if(status[i] == 0){
-            find(nums[i], i, i-1);
-        }
-    }
+    find(nums[N-1], N-1);
 
     for(int i=0; i<N; i++){
         printf("%d ", nums[i]);
@@ -30,10 +26,19 @@ int main(){
 
     for(int i=0; i<N; i++){
         printf("%d ", status[i]);
-        // if(status[i] == 0){
-        //     printf("%d\n", nums[i]);
-        //     break;
-        // }
+    }
+    printf("\n");
+
+    if(count != N-1){
+        printf("%d\n", die);
+    }
+    else{
+        for(int i=0; i<N-1; i++){
+            if(status[i] == 0){
+                printf("%d\n", nums[i]);
+                break;
+            }
+        }
     }
 
     return 0;
@@ -51,36 +56,74 @@ void bubble_sort(int* arr){
     }
 }
 
-int find(int target, int location, int right){
-    printf("hi\n");
-    int left=0;
-    int temp_count=0;
-    while(left < right){
-        int x = target-nums[left]-nums[right];
-        if(x == 0){
-            status[location] = 1;
-            status[left] = 1;
-            status[right] = 1;
-            count += 3;
-            temp_count += 1;
-            for(int i=right-1; i>=2; i--){
-                if(status[i] == 0){
-                    find(nums[i], i, i-1);
-                }
-            }
+void find(int target, int location){
+    
+    if(count == N-1){
+        return;
+    }
 
-        }
-        else if(x > 0){
-            left++;
-        }
-        else if(x < 0){
+    int left=0;
+    int right = location-1;
+    int temp_count=0;
+
+    while(left < right){
+        if(status[right] == 1){
             right--;
         }
+        else if(status[left] == 1){
+            left++;
+        }
+        else{
+            int x = target-nums[left]-nums[right];
+            if(x == 0){
+                status[location] = 1;
+                status[left] = 1;
+                status[right] = 1;
+                count += 3;
+                temp_count += 1;
+                printf("left=(%d,%d),right=(%d,%d),tar=(%d,%d)\n", left,nums[left],right,nums[right],location,nums[location]);
+                
+                for(int i=0; i<N; i++){
+                    printf("%d ", nums[i]);
+                }
+                printf("\n");
+
+                for(int i=0; i<N; i++){
+                    printf("%d ", status[i]);
+                }
+                printf("\n");
+
+                int i = location-1;
+                while(status[i] != 0 && i>=2){
+                    i--;
+                }
+                if(i >= 2){
+                    find(nums[i], i);
+                }
+
+                if(count < N-1){
+                    count -= 3;
+                    status[location] = 0;
+                    status[left] = 0;
+                    status[right] = 0;
+                    left++;
+                    right--;
+                    temp_count--;
+                }
+            }
+            else if(x > 0){
+                left++;
+            }
+            else if(x < 0){
+                right--;
+            }
+        }
+
     }
 
     if(temp_count == 0){
         die = target;
     }
     
-    return 0;
+    return ;
 }
--- I2P_1/HW/test.c
+++ I2P_1/HW/test.c
@@ -1,151 +1,15 @@
-<<<<<<< HEAD
-#include<stdio.h>
-
-int main()
-{
-    int arr[][4][4] = {
-                        
-                        {{0,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}},
-                        {{17,18,19,20},{21,22,23,24},{25,26,27,28},{29,30,31,32}}
-                        
-                    };
-
-    int i, j, k;
-    int (*p)[4][4];
-
-    p = arr;
-
-    for(i = 0; i < 2; i++)
-    {
-        //printf("Address of %d th array %u \n",i , p + i);
-        for(j = 0; j < 4; j++)
-        {
-            //printf("Address of %d th array %u \n",i , p + i);
-            for(k=0; k<4; k++){
-                printf("arr[%d][%d][%d]=%d\n", i, j, k, *(*( *(p + i) + j)+k) );
-            }
-            printf("\n");
-        }
-        printf("\n\n");
-    }
-
-    // signal to operating system program ran fine
-=======
 #include <stdio.h>
 
-int way;
-int n;
-int binary_tree[5005];
-
-
-void res(int node){
-    if (way==0){
-        printf("%d ", binary_tree[node]);
-        
-        if (2*node <= n){
-            res(2*node);
-        }
-        if (2*node+1 <= n){
-            res(2*node+1);
-        }
-    }
-    if (way==2){
-        if (2*node <= n){
-            res(2*node);
-        }
-        if (2*node+1 <= n){
-            res(2*node+1);
-        }
-        printf("%d ", binary_tree[node]);
-    }
-    if (way==1){
-        if (2*node <= n){
-            res(2*node);
-        }
-        printf("%d ", binary_tree[node]);
-        if (2*node+1 <= n){
-            res(2*node+1);
-        }
-
-    }
-<<<<<<< HEAD
-}
-
 int main(){
+
+    int arr[5] = {1,2,4,8,16};
+
+    int* const pt = &arr[1];
     
-    scanf("%d",&way);
-    scanf("%d",&n);
-    
-    for (int i=1;i<=n;i++){
-        scanf("%d", &binary_tree[i]);
-    }
+    *pt = 10;
+    pt = &arr[2];
+    printf("%d\n", *pt);
 
-    res(1);
-=======
-    return 0;
-}
 
-void go(int x, int y){
-
-    status[x][y]++;
-
-    if(flag == 1){
-        return;
-    }
-    if(x == (m-1) && y == (m-1)){
-        flag = 1;
-        return;
-    }
-    else{
-        if(grid[x][y] == 'T'){
-            start_flag = 1;
-        }
-
-        for(int i=0; i<4; i++){
-            if(valid(x+pos_x[i], y+pos_y[i])){
-                go(x+pos_x[i], y+pos_y[i]);
-            }
-        }
-    }
-}
-
-void find_end_portal(int x, int y){
-
-    status[x][y]++;
-
-    if(end_flag == 1){
-        return;
-    }
-    // if(x == 0 && y == 0){
-    //     flag = 1;
-    //     return;
-    // }
-    else{
-        if(grid[x][y] == 'T'){
-            end_flag = 1;
-            return;
-        }
-        else{
-            for(int i=0; i<4; i++){
-                if(valid(x+pos_x[i], y+pos_y[i])){
-                    find_end_portal(x+pos_x[i], y+pos_y[i]);
-                }
-            }
-        }
-    }
-}
->>>>>>> 0df81d38c7df6a8a590225e168809131c947f9dc
-
-int valid(int x, int y){
-    if(x>=0 && y>=0 && x<n && y<m){
-        if(grid[x][y] == '#'){
-            return 0;
-        }
-        else if(status[x][y] != 0){
-            return 0;
-        }
-        return 1;
-    }
->>>>>>> a80cd9f9e6e34b9f2f7ef95e850eada0d9512c8a
     return 0;
 }
