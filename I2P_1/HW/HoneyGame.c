f6f07f762c2b0f0a174cd9c09351a58565ff0fe4
weiiiii0622 <weiiiii.0622@gmail.com>
2021-12-12 16:50:13 +0800

    update Honey Game

--- I2P_1/HW/HoneyGame.c
+++ I2P_1/HW/HoneyGame.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 
 int Pooh_turn(int);
 int Piglet_turn(int);
+int status[10001][2] = {-1};
 
 int a, b;
 
@@ -13,10 +15,11 @@ int main(){
     while(T--){
         int n;
         char name[7];
+        memset(status, -1, 10001*2*sizeof(status[0][0]));
         scanf("%d %d %d %s", &n, &a, &b, name);
         if(name[1]=='o'){
-            int status = Pooh_turn(n);
-            if(status == 1){
+            int result = Pooh_turn(n);
+            if(result == 1){
                 printf("Pooh\n");
             }
             else{
@@ -24,8 +27,8 @@ int main(){
             }
         }
         else{
-            int status = Piglet_turn(n);
-            if(status == 1){
+            int result = Piglet_turn(n);
+            if(result == 1){
                 printf("Piglet\n");
             }
             else{
@@ -39,25 +42,32 @@ int main(){
 
 int Pooh_turn(int n){
     if(n <= a){
-        return 1;
+        return status[n][0] = 1;
     }
-    for(int i=1 ;i<=a; i++){
-        if(!(Piglet_turn(n-i))){
-            return 1;
+    if(status[n][0] != -1){
+        return status[n][0];
+    }
+    else{
+        for(int i=1 ;i<=a; i++){        
+            if(!(Piglet_turn(n-i))){
+                return status[n][0] = 1;
+            }
         }
     }
-    return 0;
+    return status[n][0] = 0;
 }
  
 int Piglet_turn(int n){
     if(n <= b){
-        return 1;
+        return status[n][1] = 1;
     }
-
+    if(status[n][1] != -1){
+        return status[n][1];
+    }
     for(int i=1 ;i<=b; i++){
         if(!(Pooh_turn(n-i))){
-            return 1;
+            return status[n][1] = 1;
         }
     }
-    return 0;
+    return status[n][1] = 0;
 }
--- I2P_1/HW/test.c
+++ I2P_1/HW/test.c
@@ -1,15 +1,51 @@
-#include <stdio.h>
+#include<stdio.h>
+#define num 11111
 
-int main(){
+void postorder(int);
+void inorder(int);
+void preorder(int);
 
-    int arr[5] = {1,2,4,8,16};
+int tree[num] = {0};
+int main () {
+    int x,y,i;
+    int n = 1;
 
-    int* const pt = &arr[1];
-    
-    *pt = 10;
-    pt = &arr[2];
-    printf("%d\n", *pt);
+    scanf("%d",&x);
+    scanf("%d",&y);
+    for(i = 1; i <= y; i++){
+        scanf("%d",&tree[i]);
+    }
 
+    if(x == 0){
+        preorder(1);
+    }
+    if(x == 1){
+        inorder(1);
+    }
+    if(x == 2){
+        postorder(1);
+    }
 
-    return 0;
+}
+
+void preorder(int n){
+    if(tree[n] != 0){
+        printf("%d ",tree[n]);
+        preorder(2 * n);
+        preorder(2 * n + 1);
+    }
+}
+void inorder(int n){
+    if(tree[n] != 0){
+        inorder(2 * n);
+        printf("%d ",tree[n]);
+        inorder(2 * n + 1);
+    }
+}
+void postorder(int n){
+    if(tree[n] != 0){
+        postorder(2 * n);
+        postorder(2 * n + 1);
+        printf("%d ",tree[n]);
+    }
 }
