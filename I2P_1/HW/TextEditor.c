b1557e58b663a2267ab4c24eb777be10c2654209
weiiiii0622 <weiiiii.0622@gmail.com>
2021-12-27 15:08:42 +0800

    update HW12

--- I2P_1/HW/TextEditor.c
+++ I2P_1/HW/TextEditor.c
@@ -6,13 +6,14 @@ char content[MAX_SIZE];
 char input[MAX_SIZE];
 
 char *cursor_con, *cursor_inp, *len;
+int flag = 0;
 
 void solve();
 
 int main(){
 
     fgets(input, MAX_SIZE, stdin);
-
+    //scanf("%s", input);
     solve();
     printf("%s", content);
     return 0;
@@ -22,32 +23,33 @@ void solve(){
 
     cursor_con = content;
     cursor_inp = input;
-    len = input;
+    len = content;
 
     while(*(cursor_inp) != '\0'){
 
-        if(*(cursor_inp) == '\\'){
+        if(*(cursor_inp) == '/'){
             cursor_inp += 1;
             if(*(cursor_inp) == 'b'){
                 cursor_inp += 9;
-                if((cursor_con-input) != 0){
+                
+                if((cursor_con-content) != -1){
                     int i=0;
                     while(cursor_con+i < len){
                         *(cursor_con+i) = *(cursor_con+i+1);
                         i++;
                     }
+
                     cursor_con -= 1;
                     len -= 1;
                 }
             }
             else if(*(cursor_inp) == 'n'){
                 cursor_inp += 7;
-                *(++cursor_con) = '\\';
-                *(++cursor_con) = 'n';
+                *(++cursor_con) = '\n';
             }
             else if(*(cursor_inp) == 'l'){
                 cursor_inp += 4;
-                if((cursor_con-input) != 0){
+                if((cursor_con-content) != -1){
                     cursor_con -= 1;
                 }
             }
@@ -66,10 +68,17 @@ void solve(){
                     i++;
                 }
             }
-            if((len-arr));
-            *(++cursor_con) = *(cursor_inp);
+
+            if(flag == 0){
+                flag = 1;
+                *(cursor_con) = *(cursor_inp);
+            }
+            else{
+                cursor_con += 1;
+                *(cursor_con) = *(cursor_inp);
+                len += 1;
+            }
             cursor_inp += 1;
-            len += 1;
         }
     }
 }
--- I2P_1/HW/test.c
+++ I2P_1/HW/test.c
@@ -1,12 +1,88 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-int arr[5] = {1,2,3,4,5};
+#define MAX_SIZE 500
+
+char content[MAX_SIZE];
+char input[MAX_SIZE];
+
+char *cursor_con, *cursor_inp, *len;
+int flag = 0;
+
+void solve();
 
 int main(){
-    int*pt = arr+2;
-    printf("%td\n", (int*)pt-(int*)arr);
 
+    //fgets(input, MAX_SIZE, stdin);
+    scanf("%s", input);
+    solve();
+    printf("%s", content);
     return 0;
+}
+
+void solve(){
+
+    cursor_con = content;
+    cursor_inp = input;
+    len = content;
+
+    while(*(cursor_inp) != '\0'){
+
+        if(*(cursor_inp) == '/'){
+            cursor_inp += 1;
+            if(*(cursor_inp) == 'b'){
+                cursor_inp += 9;
+                
+                if((cursor_con-content) != -1){
+                    int i=0;
+                    while(cursor_con+i < len){
+                        *(cursor_con+i) = *(cursor_con+i+1);
+                        i++;
+                    }
+
+                    cursor_con -= 1;
+                    len -= 1;
+                }
+            }
+            else if(*(cursor_inp) == 'n'){
+                cursor_inp += 7;
+                *(++cursor_con) = '\n';
+            }
+            else if(*(cursor_inp) == 'l'){
+                cursor_inp += 4;
+                if((cursor_con-content) != -1){
+                    cursor_con -= 1;
+                }
+                printf("cursor_con = %d\n\n", cursor_con-content);
+            }
+            else if(*(cursor_inp) == 'r'){
+                cursor_inp += 5;
+                if(cursor_con != len){
+                    cursor_con += 1;
+                }
+                printf("cursor_con = %d\n\n", cursor_con-content);
+            }
+        }
+        else{
+            int i=0;
+            if(len != cursor_con){
+                while(len-i > cursor_con){
+                    *(len+1-i) = *(len-i);
+                    i++;
+                }
+            }
+
+            if(flag == 0){
+                flag = 1;
+                *(cursor_con) = *(cursor_inp);
+            }
+            else{
+                cursor_con += 1;
+                *(cursor_con) = *(cursor_inp);
+                len += 1;
+            }
+            printf("add %c\n", *(cursor_con));
+            printf("cursor_con = %d\n\n", cursor_con-content);
+            cursor_inp += 1;
+        }
+    }
 }
